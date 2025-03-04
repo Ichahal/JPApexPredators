d01a7d212e09ce67a6389ccf4743c7844287f0c8
Ichahal <83983971+Ichahal@users.noreply.github.com>
2025-03-04 09:28:47 -0500

    App Completed.

--- JPApexPredators/AplexPredator.swift
+++ JPApexPredators/AplexPredator.swift
@@ -1,24 +1,74 @@
 //
 //  AplexPredators.swift
-//  JPApexPredators
+//  JPApexPredator
 //
 //  Created by Aman  Chahal on 3/4/25.
 //
 
-struct ApexPredators: Decodable{
+import SwiftUI
+import MapKit
+
+struct ApexPredator: Decodable, Identifiable{
     let id: Int
     let name: String
-    let type: String
+    let type: ApType
     let latitude: Double
     let longitude: Double
     let movies: [String]
     let movieScenes: [MovieScene]
     let link: String
     
-    struct MovieScene : Decodable{
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
+    var  location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    struct MovieScene : Decodable, Identifiable{
         let id: Int
         let movie: String
         let sceneDescription: String
     }
+    
 }
 
+
+enum ApType : String, Decodable, CaseIterable, Identifiable {
+    case all
+    case land
+    case air
+    case sea
+    
+    var id : ApType {
+        self
+    }
+    
+    var background: Color {
+        switch self {
+        case .land:
+            return .brown
+        case .air:
+            return .teal
+        case .sea:
+            return .blue
+        case .all:
+            return .black
+        }
+    }
+    
+    var icon : String {
+        switch self {
+        case .land:
+            return "leaf.fill"
+        case .air:
+            return "wind"
+        case .sea:
+            return "drop.fill"
+        case .all:
+            return "square.stack.3d.up.fill"
+        }
+    }
+    
+    }
--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -6,13 +6,126 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct PredatorDetail: View {
+    let predator: ApexPredator
+    
+    @State var position : MapCameraPosition
+    
+    @Namespace var namespace
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ScrollView{
+                ZStack(alignment: .bottomTrailing){
+                    // Background IMG
+                    Image(predator.type.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                        .overlay{
+                            LinearGradient(stops: [Gradient.Stop(color: .clear, location: 0.8),
+                                                   Gradient.Stop(color: .black, location: 1)],
+                                           startPoint: .top, endPoint: .bottom)
+                        }
+                    
+                    // Dino IMG
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width/1.5, height: geo.size.height/3.7)
+                        .scaleEffect(x: -1)
+                        .shadow(color: .black, radius: 7)
+                        .offset(y:20)
+                }
+                
+                VStack (alignment: .leading){
+                    
+                    // Dino name
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    // Current Location
+                    NavigationLink {
+                        PredatorMap(position: .camera(MapCamera(centerCoordinate: predator.location,
+                                                                distance: 1000,
+                                                                heading: 250,
+                                                                pitch: 80))
+                        )
+                        .navigationTransition(.zoom(sourceID: 1, in: namespace))
+                    } label: {
+                        Map(position: $position){
+                            Annotation(predator.name, coordinate: predator.location) {
+                                Image(systemName: "mappin.and.ellipse")
+                                    .font(.largeTitle)
+                                    .imageScale(.large)
+                                    .symbolEffect(.pulse)
+                            }
+                            .annotationTitles(.hidden)
+                        }
+                        .frame(height: 125)
+                        .overlay(alignment: .trailing){
+                            Image(systemName: "greaterthan")
+                                .imageScale(.large)
+                                .font(.title3)
+                                .padding(.trailing, 5)
+                        }
+                        .overlay(alignment: .topLeading) {
+                            Text("Current Location")
+                                .padding([.leading, .bottom], 5)
+                                .padding(.trailing, 5)
+                                .background(.black.opacity(0.33))
+                                .clipShape(.rect(bottomTrailingRadius: 15))
+                        }
+                        .clipShape(.rect(cornerRadius: 15))
+                    }
+                    .matchedTransitionSource(id: 1, in: namespace)
+
+                    // Appear in
+                    Text("Appears In:")
+                        .font(.title3)
+                        .padding(.top)
+
+                    
+                    ForEach(predator.movies, id: \.self) { movie in
+                        Text("• " + movie)
+                            .font(.subheadline)
+                    }
+                    
+                    // Movie Moments
+                    Text("Movie Moments")
+                        .font(.title)
+                        .padding(.top, 15)
+                    
+                    ForEach (predator.movieScenes) { scene in
+                        Text(scene.movie)
+                            .font(.title2)
+                            .padding(.vertical, 1)
+                        
+                        Text(scene.sceneDescription)
+                            .padding(.bottom, 15)
+                    }
+                    // link to webpage
+                    Text("Read More:")
+                        .font(.caption)
+                    
+                    Link(predator.link, destination: URL(string: predator.link)!)
+                        .font(.caption)
+                        .foregroundStyle(.blue)
+                }
+                .padding()
+                .padding(.bottom)
+                .frame(width: geo.size.width, alignment: .leading)
+            }
+        }
+        .ignoresSafeArea()
+        .toolbarBackground(.automatic)
     }
 }
 
 #Preview {
-    PredatorDetail()
-}
+    let predator = Predators().apexPredators[2]
+    NavigationStack {
+        PredatorDetail(predator: predator, position: .camera(
+            MapCamera(centerCoordinate: predator.location, distance: 30000)))
+        .preferredColorScheme(.dark)
+    }}
