be6f832f718c2ed10f9e3b972fbc9cf2abe482e4
Ichahal <83983971+Ichahal@users.noreply.github.com>
2025-03-04 01:08:48 -0500

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Aman  Chahal on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
